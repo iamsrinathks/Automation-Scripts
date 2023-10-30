2ac362cedd8fce4f9db3649a5a1647f72940e862
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2023-10-30 23:47:27 +0000

    Update rr_deploy.py

--- rr_deploy.py
+++ rr_deploy.py
@@ -1,5 +1,4 @@
 from kubernetes import dynamic, config
-from kubernetes.client import api_client
 import datetime
 
 # Define excluded namespaces
@@ -9,20 +8,23 @@ excluded_namespaces = ["kube-system", "default", "excluded-namespace-1", "exclud
 config.load_kube_config()
 
 # Create a dynamic client
-client = dynamic.DynamicClient(api_client.ApiClient())
-
-# Get a reference to the Deployment resource
-api = client.resources.get(api_version="apps/v1", kind="Deployment")
+client = dynamic.DynamicClient()
 
 # Retrieve the list of all namespaces
 all_namespaces = client.resources.get(api_version="v1", kind="Namespace").get()
 
-# Iterate through all namespaces and patch the Deployment in eligible namespaces
+# Iterate through all namespaces
 for ns in all_namespaces.items:
     namespace = ns.metadata.name
     if namespace not in excluded_namespaces:
-        deployment_manifest = api.get(name=deployment_name, namespace=namespace).to_dict()
-        deployment_manifest["spec"]["template"]["metadata"]["annotations"] = {
-            "kubectl.kubernetes.io/restartedAt": datetime.datetime.utcnow().isoformat()
-        }
-        api.patch(body=deployment_manifest, name=deployment_name, namespace=namespace)
+        # Get a reference to Deployments in the current namespace
+        deployments = client.resources.get(api_version="apps/v1", kind="Deployment", namespace=namespace).get()
+
+        # Iterate through Deployments in the current namespace and patch each one
+        for deployment in deployments.items:
+            deployment_name = deployment.metadata.name
+            deployment_manifest = client.resources.get(api_version="apps/v1", kind="Deployment", namespace=namespace, name=deployment_name).get().to_dict()
+            deployment_manifest["spec"]["template"]["metadata"]["annotations"] = {
+                "kubectl.kubernetes.io/restartedAt": datetime.datetime.utcnow().isoformat()
+            }
+            client.resources.get(api_version="apps/v1", kind="Deployment", namespace=namespace, name=deployment_name).patch(body=deployment_manifest)
