0f2c62e4903f1238af60b967317061bb6a360cf8
iamsrinathks <104845233+iamsrinathks@users.noreply.github.com>
2023-10-30 23:04:24 +0000

    Update rolling_restart.py

--- rolling_restart.py
+++ rolling_restart.py
@@ -33,4 +33,28 @@ eligible_namespaces = [ns.metadata.name for ns in namespace_list.items if ns.met
 for namespace in eligible_namespaces:
     rolling_restart(namespace)
 
+
 print("Rolling restarts in all eligible namespaces completed.")
+
+
+
+---------
+
+import subprocess
+
+# Define the excluded namespaces
+excluded_namespaces = ["kube-system", "default", "excluded-namespace-1", "excluded-namespace-2"]
+
+# Get a list of all namespaces
+all_namespaces = subprocess.check_output("kubectl get namespaces -o custom-columns=NAME:.metadata.name --no-headers", shell=True).decode("utf-8").split()
+
+# Determine eligible namespaces by excluding the ones in the exclusion list
+eligible_namespaces = [ns for ns in all_namespaces if ns not in excluded_namespaces]
+
+# Define the Deployment name
+deployment_name = "your-deployment-name"
+
+# Iterate through the eligible namespaces and perform rolling restart
+for namespace in eligible_namespaces:
+    subprocess.call(f"kubectl rollout restart deployment/{deployment_name} -n {namespace}", shell=True)
+
